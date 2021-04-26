1454ee5686cf75251b28362138404861cc818a14
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-04-26 16:31:06 +0600

    product card view adde

--- src/App.js
+++ src/App.js
@@ -1,14 +1,69 @@
-
+import React, { Component } from "react";
+import myData from "./data.json";
 import "./App.css";
-import SearchBar from "./SearchBar";
+import Card from "./Card";
+import axios from "axios";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      data: null,
+    };
+  }
 
-function App() {
-  return (
-    <div>
-      <SearchBar></SearchBar>
-    </div>
-  );
+  handleclick(event) {
+    if (event.key === "Enter") {
+      this.setState({ loading: true });
+      axios
+        .get(
+          `http://127.0.0.1:8000/deal-search/aus?keyword=${this.title.value}`
+        )
+        .then((res) => {
+          this.setState({ data: res.data.totalProductlist });
+          this.setState({ loading: false });
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="searchBarContainer">
+          <input
+            className="searchBar"
+            placeholder="Search Product"
+            type="text"
+            height="500px"
+            ref={(c) => (this.title = c)}
+            name="title"
+            onKeyDown={this.handleclick.bind(this)}
+          ></input>
+        </div>
+        <div className="productCardContainer">
+          {!this.state.loading && this.state.data != null ? (
+            this.state.data.map((prod, i) => {
+              return (
+                <Card
+                  key={i}
+                  productLink={prod.href}
+                  productName={prod.title}
+                  productPrice={prod.price}
+                  productImage={prod.image}
+                  websiteName={prod.websiteName}
+                ></Card>
+              );
+            })
+          ) : this.state.loading ? (
+            <div>loading....</div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./Card.css";
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="cardContainer">
+        <div className="ProductImage">
+          <a href={this.props.productLink}>
+            <img
+              src={this.props.productImage}
+              alt="couldn' load"
+              
+            />
+          </a>
+        </div>
+        <div className="ProductDetails">
+          <h4>{this.props.productName}</h4>
+          <p>Price: {this.props.productPrice}</p>
+          <p>Website Name: {this.props.websiteName}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
